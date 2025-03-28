7ce80fdbaa57c644027faaff89c7ed013e41ba55
Akhil Anilkumar <akhil.anilkumar@cubettech.com>
2025-03-28 12:58:44 +0530

    removed create thread from useStream

--- src/react/stream.tsx
+++ src/react/stream.tsx
@@ -643,7 +643,7 @@ export function useStream<
     ],
   );
 
-  const [threadId, onThreadId] = useControllableThreadId(options);
+  const [threadId] = useControllableThreadId(options);
 
   const [branch, setBranch] = useState<string>("");
   const [isLoading, setIsLoading] = useState(false);
@@ -784,9 +784,8 @@ export function useStream<
 
       let usableThreadId = threadId;
       if (!usableThreadId) {
-        const thread = await client.threads.create();
-        onThreadId(thread.thread_id);
-        usableThreadId = thread.thread_id;
+        console.error("Thread ID is required but not provided.");
+        return;
       }
 
       const streamMode = unique([
